1f809798df74872ebd29c10b7a6e6f03474fbab1
Diken <dikenmaharjan10@gmail.com>
2025-01-10 16:28:22 +0545

    feat: show scan qr icon in scan qr button

--- app/src/main/java/com/gurzu/myapplication/login/components/ScanQrButton.kt
+++ app/src/main/java/com/gurzu/myapplication/login/components/ScanQrButton.kt
@@ -1,5 +1,6 @@
 package com.gurzu.myapplication.login.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
@@ -8,7 +9,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.gurzu.myapplication.R
 
 @Composable
 fun ScanQrButton(modifier: Modifier = Modifier, onClick: () -> Unit) {
@@ -19,5 +22,9 @@ fun ScanQrButton(modifier: Modifier = Modifier, onClick: () -> Unit) {
     ) {
         Text("扫码登录")
         Spacer(Modifier.width(8.dp))
+        Image(
+            painter = painterResource(R.drawable.scan_qr),
+            contentDescription = "Scan QR"
+        )
     }
 }
