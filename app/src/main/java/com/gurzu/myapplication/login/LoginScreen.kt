2f0bb6eb122c7990cf4770aa7a2f888d3882813b
Diken <dikenmaharjan10@gmail.com>
2025-01-10 16:25:25 +0545

    feat: show background map in Login Screen

--- app/src/main/java/com/gurzu/myapplication/login/LoginScreen.kt
+++ app/src/main/java/com/gurzu/myapplication/login/LoginScreen.kt
@@ -1,6 +1,8 @@
 package com.gurzu.myapplication.login
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,7 +14,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.gurzu.myapplication.R
 import com.gurzu.myapplication.login.components.ForgotPasswordRow
 import com.gurzu.myapplication.login.components.LoginTextFields
 import com.gurzu.myapplication.login.components.ScanQrButton
@@ -32,22 +37,30 @@ private fun LoginScreenContent(
     onScanQr: () -> Unit,
     onSignIn: () -> Unit
 ) {
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .systemBarsPadding()
-            .padding(24.dp),
-        verticalArrangement = Arrangement.SpaceAround,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        ScanQrButton(modifier = Modifier.align(Alignment.End), onClick = onScanQr)
-        Text("立即登录以访问更快的互联网\n超过120+个位置。")
-        LoginTextFields()
-        ForgotPasswordRow()
-        SignInButton(
-            onSignIn = onSignIn
+    Box(modifier = modifier) {
+        Image(
+            modifier = Modifier.fillMaxWidth(),
+            painter = painterResource(R.drawable.login_background),
+            contentDescription = "Login Background",
+            contentScale = ContentScale.FillWidth
         )
-        Spacer(modifier = Modifier)
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .systemBarsPadding()
+                .padding(24.dp),
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            ScanQrButton(modifier = Modifier.align(Alignment.End), onClick = onScanQr)
+            Text("立即登录以访问更快的互联网\n超过120+个位置。")
+            LoginTextFields()
+            ForgotPasswordRow()
+            SignInButton(
+                onSignIn = onSignIn
+            )
+            Spacer(modifier = Modifier)
+        }
     }
 }
 
