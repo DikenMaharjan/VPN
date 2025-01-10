bd912bb83b0019e21fe88964aa2c44ca92650dd0
Diken <dikenmaharjan10@gmail.com>
2025-01-10 16:11:59 +0545

    feat: setup primary color

--- app/src/main/java/com/gurzu/myapplication/MainActivity.kt
+++ app/src/main/java/com/gurzu/myapplication/MainActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import com.gurzu.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,11 +13,14 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent {
-            MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                }
+            MyApplicationTheme(
+                darkTheme = false,
+                dynamicColor = false
+            ) {
+                LoginScreen()
             }
         }
     }
